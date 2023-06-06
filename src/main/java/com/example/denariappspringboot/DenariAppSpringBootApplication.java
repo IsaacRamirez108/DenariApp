20485577ebcd3592ddb2cd5f6573e7cb92605ade
Isaac Ramirez <isaac.ramirez7365@gmail.com>
2023-06-06 14:07:02 -0500

    updates

--- src/main/java/com/example/denariappspringboot/DenariAppSpringBootApplication.java
+++ src/main/java/com/example/denariappspringboot/DenariAppSpringBootApplication.java
@@ -1,11 +1,9 @@
 package com.example.denariappspringboot;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DenariAppSpringBootApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(DenariAppSpringBootApplication.class, args);
     }
