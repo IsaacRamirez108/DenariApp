bcee64c14b3a953bf6f5a09161e249fb8a1e6a0f
Isaac Ramirez <isaac.ramirez7365@gmail.com>
2023-06-07 11:09:25 -0500

    updates

--- src/main/java/com/example/denariappspringboot/repositories/UserRepository.java
+++ src/main/java/com/example/denariappspringboot/repositories/UserRepository.java
@@ -4,5 +4,5 @@ import com.example.denariappspringboot.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findById(long id);
+//    User findById(long id);
 }
